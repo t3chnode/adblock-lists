752fdb18e08b9f35fbbbc0900d5f7545cff7d4b3
t3chnode <caleb94587@hotmail.com>
2021-10-02 16:43:21 +0800

    Auto Update: 2021-10-02

--- updateRepos.sh
+++ updateRepos.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-#adlistsGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/adlists.txt"
-#malwareGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/malware.txt"
-#phishingGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/phishing.txt"
-#seasonscamsGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/seasonalscams.txt"
-#trackingGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/tracking.txt"
-#unclassifiedGitURL="https://raw.githubusercontent.com/seedlessnetwork/advocado/master/blocklists/unclassified.txt"
 
-sourceDir="/home/anon/adlists/sourceLists/"
-uploadDir="/home/anon/adlists/uploadLists/"
+#sourceDir="/home/anon/adlists/sourceLists/"
+#uploadDir="/home/anon/adlists/uploadLists/"
 cd /home/anon/adlists/
 unset IFS
-for x in $(ls -Ut $sourceDir | xargs -n 1 basename) ; do
+for x in $(ls -Ut ./sourceLists | xargs -n 1 basename) ; do
     URLs=()
     while IFS= read -r line; do
         URLs+=("$line")
-    done <$sourceDir$x
+    done <"./sourceLists/$x"
     #Uncomment for debugging
     echo -e "Now doing $x"
     fullList=()
@@ -24,7 +18,7 @@ for x in $(ls -Ut $sourceDir | xargs -n 1 basename) ; do
         fullList+=$(curl -s "$z")
     done
     fullListClean=$(echo $fullList | sort -u | sed '/^#/d' | sed '/^ /d' | sed '/^$/d')
-    echo $fullListClean > $uploadDir$x
+    echo $fullListClean > "./uploadLists/$x"
     unset IFS
 done
 git add .
